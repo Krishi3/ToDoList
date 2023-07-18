ba447fdf315fa5d4a4408f63a4cb2752a6cc638b
Krishi <krishi3dalal@gmail.com>
2023-07-18 11:25:48 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Krishi Dalal on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
